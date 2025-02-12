22357e6db7cf507e877423deea16004d06b9fd81
Sourabh-Bhakar5228 <golubhakar5228@gmail.com>
2025-02-12 12:22:47 +0530

    first commit

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "099bbbb812f2e49338e32a0db346eccd";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
